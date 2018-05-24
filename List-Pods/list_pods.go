e46faf42d3ab476e7251c0dbca67940fe90bfe35
Meghna Singh <singhmeghna79@gmail.com>
2018-05-24 18:43:40 +0530

    Update list_pods.go

--- List-Pods/list_pods.go
+++ List-Pods/list_pods.go
@@ -9,7 +9,7 @@ import (
 
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	//_ "k8s.io/client-go/plugin/pkg/client/auth"
 	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
